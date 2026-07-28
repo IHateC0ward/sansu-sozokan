e1ce6bccdc69512aeee6445c81f9a82091d2230f
IHateC0ward <937648864@qq.com>
2026-07-28 19:55:40 +0900

    Update next.config.mjs

--- sansu-sozokan/next.config.mjs
+++ sansu-sozokan/next.config.mjs
@@ -1,13 +1,9 @@
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isGitHubActions ? "/sansu-sozokan" : "",
-  assetPrefix: isGitHubActions ? "/sansu-sozokan/" : "",
 };
 
 export default nextConfig;
